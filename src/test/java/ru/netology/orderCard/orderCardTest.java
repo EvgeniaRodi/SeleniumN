452c7d356852bbf8a8406a083109e8571ce415a7
Rodionova Evgenia <jenechkajj353@gmail.com>
2021-07-27 21:38:22 +0300

    test

--- src/test/java/ru/netology/orderCard/orderCardTest.java
+++ src/test/java/ru/netology/orderCard/orderCardTest.java
@@ -5,15 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 
 public class orderCardTest {
 
